bfaac445e9162117068b6efd8fe5d5e4eae7e625
Bin <45880588+Alicebond@users.noreply.github.com>
2025-01-14 17:35:00 +0800

    populate db

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,15 +5,57 @@ const { Client } = reequire("pg");
 require("dotenv").config();
 
 const SQL = `
-CREATE TABLE books (
+CREATE TABLE book (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  bookname VARCHAR(255),
-  author VARCHAR(255),
-  added DATE
+  title VARCHAR(255),
+  ISBN VARCHAR(13),
+  pages INT
+  added DATE,
+  reading BOOLEAN DEFAULT FALSE,
+  read BOOLEAN DEFAULT FALSE,
+);
+
+CREATE TABLE author (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+  first_name VARCHAR(255),
+  last_name VARCHAR(255), 
+  description TEXT,
+  date_of_birth DATE,
+  date_of_death DATE,
+  name GENERATED ALWAYS AS CONCAT(first_name, last_name) STORED,
+);
+
+CREATE TABLE genre (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255), 
+)
+
+CREATE TABLE book_author (
+  book_id INT REFERENCES book(id) ON DELETE RESTRICT,
+  author_id INT REFERENCES authors(id) ON DELETE CASCADE,
+  PRIMARY KEY(book_id, author_id)
+)
+
+CREART TABLE bood_genre (
+  book_id INT REFERENCS book(id) ON DELETE RESTRICT,
+  genre_id INT REFERENCS genre(id) ON DELETE RESTRICT, 
+  PRIMARY KEY (book_id, genre_id)
 );
   
-INERT INTO books (bookname, author, added)
-VALUES ('Wonder', 'R. J. Palacio', now());`;
+INERT INTO book (title, ISBN, pages, added, read)
+VALUES ('Wonder', '9780375969027', 320, now(), TRUE);
+
+INSERT INTO author (first_name, last_name, date_of_birth)
+VALUES ('R. J.', 'Palacio', '1963-07-13');
+
+INSERT INTO genre (name)
+VALUES ('Young Adult 青年文学', 'Fiction 虚构作品', 'Russian Literature 俄罗斯文学', 'Fantasy 奇幻文学');
+
+INSERT INTO book_author (book_id, author_id) VALUES (1, 1);
+
+INSERT INTO book_genre (book_id, genre_id) VALUES (1, 1);
+
+INSERT INTO book_genre (book_id, genre_id) VALUES (1, 2);`;
 
 async function main() {
   console.log("Seeding...");
