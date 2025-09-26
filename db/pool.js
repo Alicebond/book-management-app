e8a2ae2091770f80b87d36d7952c02de3f14f14d
Bin <45880588+Alicebond@users.noreply.github.com>
2025-09-26 16:27:29 -0400

    config database for deployment

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+//////////////////////////////////
+// Localhost
+// module.exports = new Pool({
+//   host: "localhost",
+//   user: process.env.USERNAME,
+//   database: "book_inventory",
+//   password: process.env.PASSWORD,
+//   port: 5432,
+// });
+///////////////////////////////////
+
+///////////////////////////////////
+// Noen Database
 module.exports = new Pool({
-  host: "localhost",
-  user: process.env.USERNAME,
-  database: "book_inventory",
-  password: process.env.PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
+//////////////////////////////////
